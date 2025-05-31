78c5b874a63b6ae4461121dd4bb274568e618f55
DoHoangLong2004 <longdh204@gmail.com>
2025-05-31 11:54:36 +0700

    Cài đặt game chạy trên điện thoại và cài 60fps

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60; // Set the target frame rate to 60 FPS
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
