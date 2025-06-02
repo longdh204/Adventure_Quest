bbd7e6615bb4ba304f5d577401ab8c93e92c3f5f
DoHoangLong2004 <longdh204@gmail.com>
2025-06-02 14:46:30 +0700

    Tạo slider bar hp và xp và tính toán để khi quái chạm vào người sẽ trừ máu trên slider bar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,14 +12,34 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float detectionRadius = 5f; // ban kinh phat hien va cham
     [SerializeField] private GameObject weapon;
     [SerializeField] private float rotationSpeed = 100f; // thoi gian giua cac lan tan cong
+    [SerializeField] private Slider healthSlider; // thanh mau cua nhan vat
+    [SerializeField] private Slider manaSlider; // thanh mau cua nhan vat
+    [SerializeField] private TextMeshProUGUI hp;
+    [SerializeField] private TextMeshProUGUI mp;
+    private float maxHealth = 100f; // Giá trị tối đa sức khỏe
+    private float currentHealth;
+
+    private float maxMana = 100f; // Giá trị tối đa năng lượng
+    private float currentMana;
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth; // Khởi tạo sức khỏe hiện tại
+        currentMana = maxMana; // Khởi tạo năng lượng hiện tại
+
+        healthSlider.maxValue = maxHealth; // Thiết lập giá trị tối đa cho thanh máu
+        healthSlider.value = currentHealth; // Thiết lập giá trị hiện tại cho thanh máu
+
+        manaSlider.maxValue = maxMana; // Thiết lập giá trị tối đa cho thanh năng lượng
+        manaSlider.value = currentMana; // Thiết lập giá trị hiện tại cho thanh năng lượng
     }
     private void Update()
     {
         CheckForEnemies();
         RotationWeapon();
+        hp.text = $"{currentHealth}/{maxHealth}";
+        mp.text = $"{currentMana}/{maxMana}";
+
     }
     private void FixedUpdate()
     {
@@ -45,6 +67,27 @@ public class PlayerController : MonoBehaviour
         if (weapon != null)
         {
             weapon.transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void TotalHealthPlayer(float damage)
+    {
+        currentHealth -= damage; // Giảm sức khỏe hiện tại theo lượng sát thương
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // Đảm bảo sức khỏe không âm
+        healthSlider.value = currentHealth; // Cập nhật thanh máu
+
+        if(currentHealth <= 0)
+        {
+            Debug.Log("Nhân vật đã chết");
+            //Die(); // Gọi hàm xử lý khi nhân vật chết
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Chỉ xử lý khi chính Player va chạm với Enemy
+        if (gameObject.CompareTag("Player") && collision.CompareTag("Enemy"))
+        {
+            TotalHealthPlayer(10f);
         }
     }
 }
