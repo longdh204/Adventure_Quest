0eaed3b58e63e33bba3adb485b72ad6d42f8784d
DoHoangLong2004 <longdh204@gmail.com>
2025-06-07 09:46:02 +0700

    Tạo ra con pet và con pet đó sẽ luôn quay mạt về hướng con quái gần nhất và lật được mặt lại

---
+++ Assets/Scripts/Player/PetController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetController : MonoBehaviour
+{
+    private Transform enemy;
+    [SerializeField] private float rotationSpeed = 5f;
+
+    void Start()
+    {
+        // Tìm kẻ địch trong scene
+        enemy = GameObject.FindGameObjectWithTag("Enemy").transform; // Giả sử kẻ địch có tag là "Enemy"
+    }
+
+    void Update()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (enemies.Length > 0)
+        {
+            // Tìm enemy gần nhất
+            enemy = enemies[0].transform;
+            float minDist = Vector3.Distance(transform.position, enemy.position);
+            foreach (var e in enemies)
+            {
+                float dist = Vector3.Distance(transform.position, e.transform.position);
+                if (dist < minDist)
+                {
+                    minDist = dist;
+                    enemy = e.transform;
+                }
+            }
+            // Quay về enemy gần nhất
+            Vector3 direction = enemy.position - transform.position;
+            direction.z = 0;
+            Quaternion rotation = Quaternion.LookRotation(Vector3.forward, direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+            // Lật mặt pet khi quay về bên trái hoặc phải
+            if (direction.x < 0)
+                transform.localScale = new Vector3(-1, 1, 1); // Lật mặt sang trái
+            else
+                transform.localScale = new Vector3(1, 1, 1);  // Quay mặt sang phải
+        }
+    }
+}
