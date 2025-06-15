268fcb6586b5e441f19f9cd66cc36774d02781cc
DoHoangLong2004 <longdh204@gmail.com>
2025-06-15 22:40:11 +0700

    thay đổi hình ảnh nhân vật khi ấn button mặc

--- Assets/Scripts/Managers/ShopController.cs
+++ Assets/Scripts/Managers/ShopController.cs
@@ -8,10 +8,12 @@ public class ShopController : MonoBehaviour
     private PlayerController playerController;
     public Image btnBuyDev, btnBuyPop, btnBuySmirk, btnBuySnark; //mua
     public Image btnWearDev, btnWearPop, btnWearSmirk, btnWearSnark; //mac
+    public SpriteRenderer spriteRendererPlayer; // Hình ảnh của người chơi
+    public Sprite Dev, Pop, Smirk, Snark; //hinh anh cac trang phuc
     void Start()
     {
         playerController = FindObjectOfType<PlayerController>();
-        
+
         btnBuyPop.GetComponent<Button>().onClick.AddListener(BuyPop);
         btnWearPop.gameObject.SetActive(false); // Ẩn hình mặc lúc đầu
 
@@ -20,12 +22,28 @@ public class ShopController : MonoBehaviour
 
         btnBuySnark.GetComponent<Button>().onClick.AddListener(BuySnark);
         btnWearSnark.gameObject.SetActive(false); // Ẩn hình mặc lúc đầu
+
+        btnWearDev.GetComponent<Button>().onClick.AddListener(WearDev);
+        btnWearPop.GetComponent<Button>().onClick.AddListener(WearPop);
+        btnWearSmirk.GetComponent<Button>().onClick.AddListener(WearSmirk);
+        btnWearSnark.GetComponent<Button>().onClick.AddListener(WearSnark);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    public void WearDev()
+    {
+        if (spriteRendererPlayer != null && Dev != null)
+        {
+            spriteRendererPlayer.sprite = Dev; // Đổi hình ảnh của người chơi sang Dev
+        }
+        else
+        {
+            Debug.LogWarning("SpriteRenderer không được gán cho ShopController.");
+        }
     }
     public void BuyPop()
     {
@@ -40,6 +58,19 @@ public class ShopController : MonoBehaviour
             Debug.Log("Không đủ candy để mua Pop :(( hic");
         }
     }
+public void WearPop()
+{
+    Debug.Log("WearPop được gọi");
+    if (spriteRendererPlayer != null && Pop != null)
+    {
+        spriteRendererPlayer.sprite = Pop;
+        Debug.Log("Sprite đã được thay đổi thành: " + Pop.name);
+    }
+    else
+    {
+        Debug.LogWarning("SpriteRenderer hoặc Pop đang là null");
+    }
+}
     public void BuySmirk()
     {
         if (playerController.totalCandyCurrent >= 1)
@@ -53,6 +84,17 @@ public class ShopController : MonoBehaviour
             Debug.Log("Không đủ candy để mua Smirk :(( hic");
         }
     }
+    public void WearSmirk()
+    {
+        if (spriteRendererPlayer != null && Smirk != null)
+        {
+            spriteRendererPlayer.sprite = Smirk; // Đổi hình ảnh của người chơi sang Smirk
+        }
+        else
+        {
+            Debug.LogWarning("SpriteRenderer không được gán cho ShopController.");
+        }
+    }
     public void BuySnark()
     {
         if (playerController.totalCandyCurrent >= 1)
@@ -64,6 +106,17 @@ public class ShopController : MonoBehaviour
         else
         {
             Debug.Log("Không đủ candy để mua Snark :(( hic");
+        }
+    }
+    public void WearSnark()
+    {
+        if (spriteRendererPlayer != null && Snark != null)
+        {
+            spriteRendererPlayer.sprite = Snark; // Đổi hình ảnh của người chơi sang Snark
+        }
+        else
+        {
+            Debug.LogWarning("SpriteRenderer không được gán cho ShopController.");
         }
     }
 }
